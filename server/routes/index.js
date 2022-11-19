36749e64fc938f9da7989a77654d850705108feb
marmolpen3 <marmolpen3@alum.us.es>
2022-11-19 18:26:26 +0100

    feat: added CRUD funtions for report model

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const hello = require('./hello').default;
 const users = require('./users').default;
+const reports = require('./reports').default;
 
 module.exports = {
     hello,
-    users
+    users,
+    reports
   };
