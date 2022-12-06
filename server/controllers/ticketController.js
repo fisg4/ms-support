d04a78d7eafec2069c7c638d3ebe91d1203dd62a
alemunara <alemunara@alum.us.es>
2022-12-06 20:12:05 +0100

    feat: initial API CRUD for tickets

--- server/controllers/ticketController.js
+++ server/controllers/ticketController.js
@@ -1,101 +1,82 @@
 const Ticket = require("../models/ticket");
 const debug = require('debug');
 
-const ticketDb = [
-    {
-      userId: '1',
-      text: 'Este es un mensaje de prueba',
-      id: '1',
-      date: '2022/11/17 18:26:00',
-      status: 'ongoing',
-      priority: 'medium'
-    },
-    {
-      userId: '2',
-      text: 'Este es otro mensaje de prueba',
-      id: '2',
-      date: '2022/11/19 18:30:25',
-      status: 'rejected',
-      priority: 'low'
-    }
-];
 
-const getAllTickets = async (req, res, next) => {
+const getAllTickets = async (request, response, next) => {
     try {
         const result = await Ticket.find();
-        res.send(result.map((ticket) => ticket.cleanup()));
+        response.send(result.map((ticket) => ticket.cleanup()));
     } catch (error) {
-        debug("DB problem", error);
-        res.sendStatus(500);
+        debug("Database problem", error);
+        response.sendStatus(404).send({error: error.message});
     }
 };
 
-const getTicketById = (req, res, next) => {
-    var ticketId = req.params.id;
-
-    var ticket = ticketDb.find(t => {
-        return t.id === ticketId;
-    });
-
-    if (ticket) {
-        res.send(ticket);
-    } else {
-        res.sendStatus(404);
+const getTicketById = async (request, response, next) => {
+    try {
+        var ticketId = request.params.id;
+        const result = await Ticket.findById(ticketId);
+        response.send(result.cleanup());
+    } catch (error) {
+        debug("Database problem", error);
+        response.sendStatus(404).send({error: error.message});
     }
 };
 
-const createTicket = async (req, res, next) => {
-    var {title, text} = req.body;
+const createTicket = async (request, response, next) => {
+    const {authorId, songId, title, text, priority} = request.body;
 
-    const ticket = new Ticket({
-        title,
-        text
-    });
+    const createDate = Date.now();
+
+    const ticket = new Ticket({authorId, songId, title, text, priority, createDate});
 
     try {
         await ticket.save();
-        return res.sendStatus(201);
+        return res.status(201).send(ticket.cleanup());
     } catch (error) {
         if (error.errors) {
             debug("Validation problem when saving");
-            res.status(400).send({error: error.message});
+            response.status(400).send({error: error.message});
         } else {
-            debug("DB problem", error);
-            res.sendStatus(500);
+            debug("Database problem", error);
+            response.sendStatus(500);
         }
     }
 };
 
-const updateTicket = (req, res, next) => {
-    var ticketId = req.params.id;
+const updateTicket = async (request, response, next) => {
+    const ticketId = request.params.id;
 
-    const ticket = ticketDb.find(t => {
-        return t.id === ticketId;
-    });
+    try {
+        ticket = await Ticket.findById(ticketId);
 
-    if (ticket) {
-        var ticketUpdated = req.body;
-        const index = ticketDb.indexOf(ticket);
-        ticketDb[index].text = ticketUpdated.text;
-        res.send(ticketDb[index]);
-    } else {
-        res.sendStatus(404);
+        ticketId.reviewerId = request.body.reviewerId;
+        ticket.status = request.body.status;
+        ticket.priority = request.body.priority;
+        ticket.updateDate = Date.now();
+
+        ticketUpdated = await ticket.save();
+
+        response.send(ticketUpdated.cleanup());
+    } catch (error) {
+        if (error.errors) {
+            debug("Validation problem when updating");
+            response.status(400).send({error: error.message});
+        } else {
+            debug("Database problem", error);
+            response.sendStatus(500);
+        }
     }
 };
 
-const deleteTicket = (req, res, next) => {
-    const ticketId = req.params.id;
-
-    const ticket = ticketDb.find(t => {
-        return t.id === ticketId;
-    });
-
-    if (ticket) {
-        const index = ticketDb.indexOf(ticket);
-        ticketDb.splice(index, 1);
-        res.sendStatus(200);
-    } else {
-        res.sendStatus(404);
+const deleteTicket = async (request, response, next) => {
+    try {
+        const ticketId = request.params.id;
+        await Ticket.findByIdAndDelete(ticketId);
+        response.sendStatus(204);
+    } catch (error) {
+        debug("Database problem", error);
+        response.status(404).send(error.message);
     }
 };
 
