84ab0e5b87b675aef2b3688220bff40e34cfe6e1
marmolpen3 <marmolpen3@alum.us.es>
2022-12-18 14:22:57 +0100

    chore: add integration tests configuration

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -159,13 +159,12 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "/server/tests/integration/"
-  ],
+  // testPathIgnorePatterns: [
+  //   "\\\\node_modules\\\\"
+  // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
-  // testRegex: [],
+  testRegex: ["/server/tests/integration/"],
 
   // This option allows the use of a custom results processor
   // testResultsProcessor: undefined,
