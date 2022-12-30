bcb6ec6add896fd6711e64473a66ceaa5a77b43b
marmolpen3 <marmolpen3@alum.us.es>
2022-12-30 20:40:50 +0100

    chore: add jsonwebtoken dependence

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -4,7 +4,7 @@ const ExtractJwt = require("passport-jwt").ExtractJwt;
 
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = "d997b86a580f1808d094e7e76baee30c48a87d256a06c169769d62033631ac812dfa8f2570e20588128f58357aaf77b936bf2e36832fe03f41ce50be79319082";
+opts.secretOrKey = process.env.JWT_SECRET;
 
 passport.use(
     "admin",
